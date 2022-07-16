b0f97ab7470fe1c894f026381d66a083e0872530
Pritom Sarker <pritoms@gmail.com>
2022-07-16 11:42:48 -0400

    Update bytes_tokenizer.py

--- Deepnotes/bytes_tokenizer.py
+++ Deepnotes/bytes_tokenizer.py
@@ -19,21 +19,6 @@ class BytesTokenizer:
 tokenizer = BytesTokenizer()
 
 def encode(string, max_len=None, return_type='tensor'):
-    """
-    Encode a string into a list of integers.
-
-    Args:
-        string (str): The string to encode.
-        max_len (int, optional): Maximum length of the encoded list.
-            If the string is longer than this, it will be truncated.
-            If the string is shorter than this, it will be padded with zeros.
-            Defaults to None.
-        return_type (str, optional): The type of object to return.
-            Must be either 'tensor' or 'numpy'. Defaults to 'tensor'.
-
-    Returns:
-        encoded_string (torch.Tensor or np.ndarray): The encoded string.
-    """
     utf8_bytes = string.encode('utf-8')
     encoded_string = tokenizer.encode(utf8_bytes)
     if max_len is not None:
@@ -42,29 +27,15 @@ def encode(string, max_len=None, return_type='tensor'):
         elif len(encoded_string) < max_len:
             encoded_string = np.pad(encoded_string, (0, max_len - len(encoded_string)), 'constant', constant_values=0)
     if return_type == 'tensor':
-        return torch.from_numpy(encoded_string)
+        return torch.LongTensor(encoded_string)
     elif return_type == 'numpy':
         return encoded_string
     else:
         raise ValueError('return_type must be either \'tensor\' or \'numpy\'')
 
 def decode(input_ids, return_type='string'):
-    """
-    Decode a list of integers into a string.
-
-    Args:
-        input_ids (torch.Tensor or np.ndarray): The ids corresponding to encoded string.
-        return_type (str, optional): The type of object to return.
-            Must be either 'string' or 'bytes'. Defaults to 'string'.
-
-    Returns:
-        string (str or bytes): The decoded string.
-    """
-    # Conver the `input_ids` to a numpy array if necessary
-    if type(input_ids) == torch.Tensor:
+    if type(input_ids) == torch.LongTensor:
         input_ids = input_ids.numpy()
-
-    # Decode the bytes into a string
     decoded_bytes = tokenizer.decode(input_ids)
     if return_type == 'string':
         return decoded_bytes.decode('utf-8')
